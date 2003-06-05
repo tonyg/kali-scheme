05369b38c5df84f8dd80c8a3afc5b111577989a4
gasbichl <none@none>
2003-06-05 15:36:44 +0000

    Subject: Removed support for restart-vm-w/continue2 [s48-tuebingen @ 379]
    This attempt is now obsoleted by restart-vm-w/continue3.

--- c/asm-glue.c
+++ c/asm-glue.c
@@ -39,7 +39,6 @@ s48_provide_asm_values(s48_value asm_vector)
   extern long s48_native_bitwise_and;
   extern long s48_native_bitwise_ior;
   extern long s48_native_bitwise_xor;
-  extern long s48_restart_vm2();
   extern long s48_restart_vm3_pop_0;
   extern long s48_restart_vm3_pop_1;
   extern long s48_restart_vm3_pop_2;
@@ -75,10 +74,9 @@ s48_provide_asm_values(s48_value asm_vector)
   S48_VECTOR_SET(asm_vector, 23, s48_enter_fixnum((long) &s48_native_bitwise_and));
   S48_VECTOR_SET(asm_vector, 24, s48_enter_fixnum((long) &s48_native_bitwise_ior));
   S48_VECTOR_SET(asm_vector, 25, s48_enter_fixnum((long) &s48_native_bitwise_xor));
-  S48_VECTOR_SET(asm_vector, 26, s48_enter_fixnum((long) &s48_restart_vm2));
-  S48_VECTOR_SET(asm_vector, 27, s48_enter_fixnum((long) &s48_restart_vm3_pop_0));
-  S48_VECTOR_SET(asm_vector, 28, s48_enter_fixnum((long) &s48_restart_vm3_pop_1));
-  S48_VECTOR_SET(asm_vector, 29, s48_enter_fixnum((long) &s48_restart_vm3_pop_2));
+  S48_VECTOR_SET(asm_vector, 26, s48_enter_fixnum((long) &s48_restart_vm3_pop_0));
+  S48_VECTOR_SET(asm_vector, 27, s48_enter_fixnum((long) &s48_restart_vm3_pop_1));
+  S48_VECTOR_SET(asm_vector, 28, s48_enter_fixnum((long) &s48_restart_vm3_pop_2));
   return S48_UNSPECIFIC;
 }
 
