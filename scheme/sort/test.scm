75aee194543343dc0ba02f9dc2f192938f2d5e24
sperber <none@none>
2004-03-26 17:39:21 +0000

    Subject: Make sort test suite work [s48-tuebingen @ 647]

--- scheme/sort/test.scm
+++ scheme/sort/test.scm
@@ -1,27 +1,17 @@
 ;;; Little test harness, 'cause I'm paraoid about tricky code.
-;;; It's scsh specific -- Scheme 48 random-number stuff & the mail-notification
-;;; stuff.
-
-(define r (make-random 42))
-(define (rand n) (modulo (r) n))
+;;; ,open i/o srfi-27 sort list-merge-sort vector-insertion-sort vector-utils 
 
 ;;; For testing stable sort -- 3 & -3 compare the same.
 (define (my< x y) (< (abs x) (abs y)))
 
 (define (unstable-sort-test v) ; quick & heap vs simple insert
   (let ((v1 (vector-copy v))
-	(v2 (vector-copy v))
-	(v3 (vector-copy v))
-	(v4 (vector-copy v)))
-    (quick-sort!   < v1)
-    (quick-sort3!  - v1)
-    (heap-sort!    < v2)
-    (insert-sort!  < v3)
+	(v2 (vector-copy v)))
+    (heap-sort!    < v1)
+    (insert-sort!  < v2)
     (and (or (not (equal? v1 v2))
-	     (not (equal? v1 v3))
-	     (not (equal? v1 v4))
 	     (not (vector-sorted? < v1)))
-	 (list v v1 v2 v3 v4))))
+	 (list v v1 v2))))
 
 (define (stable-sort-test v) ; insert, list & vector merge sorts
   (let ((v1 (vector-copy v))
@@ -40,18 +30,13 @@
   (let lp ((i 0))
     (let ((i (cond ((= i 1000)
 		    (write-char #\.)
-		    (force-output)
+		    (force-output (current-output-port))
 		    0)
 		   (else (+ i 1))))
-	  (v (random-vector (rand max-size))))
+	  (v (random-vector (random-integer max-size))))
       (cond ((unstable-sort-test v) => (lambda (x) (cons 'u x)))
 	    ((stable-sort-test   v) => (lambda (x) (cons 's x)))
 	    (else (lp i))))))
-
-(define (test-n-mail max-size)
-  (let ((losers (do-test max-size))
-	(email-address "shivers@cc.gatech.edu"))
-    (run (mail -s "sort lost" ,email-address) (<< ,losers))))
 
 (define (random-vector size)
   (let ((v (make-vector size)))
@@ -62,4 +47,4 @@
   (let ((half (quotient range 2)))
     (do ((i (- (vector-length v) 1) (- i 1)))
 	((< i 0))
-      (vector-set! v i (- (rand range) half)))))
+      (vector-set! v i (- (random-integer range) half)))))
