e3605f6156c5ce768223c1e62abbaa74065947b2
sperber <none@none>
2004-08-02 09:24:35 +0000

    Subject: More portable specification of dynamic libraries, Windows [s48-tuebingen @ 781]
    This is the Windows part of rev 771: We append ".dll" to the file-name
    argument of shared_object_dlopen if the caller wants us to.

--- c/win32/dynlink.c
+++ c/win32/dynlink.c
@@ -10,17 +10,37 @@
 #include "io.h"
 
 static s48_value
-shared_object_dlopen(s48_value name)
+shared_object_dlopen(s48_value name, s48_value complete_name_p)
 {
+  S48_DECLARE_GC_PROTECT(1);
   HINSTANCE handle;
   s48_value res;
+  s48_value full_name;
+
+  S48_GC_PROTECT_1(name);
+
+  if (!S48_EQ(S48_FALSE, complete_name_p))
+    {
+      size_t len = S48_STRING_LENGTH(name);
+      full_name = s48_make_string(len + 4, '\0');
+      memcpy(S48_UNSAFE_EXTRACT_STRING(full_name),
+	     S48_UNSAFE_EXTRACT_STRING(name),
+	     len);
+      memcpy(S48_UNSAFE_EXTRACT_STRING(full_name) + len,
+	     ".dll",
+	     4);
+    }
+  else
+    full_name = name;
   
-  handle = LoadLibrary(s48_extract_string(name));
+  handle = LoadLibrary(s48_extract_string(full_name));
   if (handle == NULL)
     s48_raise_string_os_error(ps_error_string(GetLastError()));
 
   res = S48_MAKE_VALUE(HINSTANCE);
   S48_SET_VALUE(res, HINSTANCE, handle);
+
+  S48_GC_UNPROTECT();
 
   return res;
 }
