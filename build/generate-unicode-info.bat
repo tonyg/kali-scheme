ccc773362f05e9930733fd27af648fc3e362ca64
sperber <none@none>
2005-01-09 11:29:38 +0000

    Subject: Unicode info generation on Windows [s48-tuebingen @ 966]
    - use only LF linebreaks on UnicodeData.txt
      (otherwise, the CRs show up as extra unrecognized characters)
    - add generation script

---
+++ build/generate-unicode-info.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set runnable=%~1
+
+echo ,bench > build\generate-unicode-info.input
+echo ,batch >> build\generate-unicode-info.input
+echo ,open finite-types destructuring define-record-types bitwise >> build\generate-unicode-info.input
+echo ,open (subset srfi-13 (string-index)) (subset srfi-1 (find)) srfi-23 >> build\generate-unicode-info.input
+echo ,load scheme/big/compact-table.scm scheme/env/unicode-category.scm  >> build\generate-unicode-info.input
+echo ,load scheme/link/unicode-data.scm >> build\generate-unicode-info.input
+echo (create-unicode-tables "build/UnicodeData.txt" "scheme/env/unicode-info.scm") >> build\generate-unicode-info.input
+
+%runnable% -h 6000000 < build\generate-unicode-info.input
