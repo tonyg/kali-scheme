d232346945d595e004eca0be2cedda126ebacccb
sperber <none@none>
2005-03-08 17:19:53 +0000

    Subject: Make replacement procedure for character-map stuff more robust [s48-tuebingen @ 1004]
    Add another level of indirection, so that clients who have stored the
    definition of, say, CHAR-UPCASE (like the reader) will get the changed
    definition once the Unicode stuff is loaded.

--- scheme/rts/charmap.scm
+++ scheme/rts/charmap.scm
@@ -2,22 +2,34 @@
 
 ; Character maps, ASCII-only version
 
+; Enable us to change the semantics afterwards (see the bottom of this file)
 (define (char-whitespace? c)
+  (char-whitespace?-proc c))
+(define (char-whitespace?-proc c)
   (if (memq (char->ascii c) ascii-whitespaces) #t #f))
 
 (define (char-lower-case? c)
+  (char-lower-case?-proc c))
+(define (char-lower-case?-proc c)
   (and (char>=? c #\a)
        (char<=? c #\z)))
 
+
 (define (char-upper-case? c)
+  (char-upper-case?-proc c))
+(define (char-upper-case?-proc c)
   (and (char>=? c #\A)
        (char<=? c #\Z)))
 
 (define (char-numeric? c)
+  (char-numeric?-proc c))
+(define (char-numeric?-proc c)
   (and (char>=? c #\0)
        (char<=? c #\9)))
 
 (define (char-alphabetic? c)
+  (char-alphabetic?-proc c))
+(define (char-alphabetic?-proc c)
   (or (char-upper-case? c)
       (char-lower-case? c)))
 
@@ -39,6 +51,8 @@
 	 c))))
 
 (define (char-upcase c)
+  (char-upcase-proc c))
+(define (char-upcase-proc c)
   (string-ref upcase-map (char->ascii c)))
 
 (define downcase-map
@@ -49,6 +63,8 @@
 	 c))))
 
 (define (char-downcase c)
+  (char-downcase-proc c))
+(define (char-downcase-proc c)
   (string-ref downcase-map (char->ascii c)))
 
 (define (char-ci-compare pred)
@@ -69,11 +85,11 @@
 				  lower-case?
 				  upcase
 				  downcase)
-  (set! char-alphabetic? alphabetic?)
-  (set! char-numeric? numeric?)
-  (set! char-whitespace? whitespace?)
-  (set! char-upper-case? upper-case?)
-  (set! char-lower-case? lower-case?)
-  (set! char-upcase upcase)
-  (set! char-downcase downcase))
+  (set! char-alphabetic?-proc alphabetic?)
+  (set! char-numeric?-proc numeric?)
+  (set! char-whitespace?-proc whitespace?)
+  (set! char-upper-case?-proc upper-case?)
+  (set! char-lower-case?-proc lower-case?)
+  (set! char-upcase-proc upcase)
+  (set! char-downcase-proc downcase))
 
