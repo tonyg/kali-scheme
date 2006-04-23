66475d15763118abefcbd8eb1e3ec9e3bd461a8e
sperber <none@none>
2006-04-23 13:06:46 +0000

    Subject: Add implementation of SRFI 74 (Octet-Addressed Binary Blocks) [s48-tuebingen @ 1524]

--- scheme/srfi/packages.scm
+++ scheme/srfi/packages.scm
@@ -104,7 +104,8 @@
 	srfi-23 srfi-25 srfi-26 srfi-27 srfi-28
 	srfi-31 srfi-34 srfi-35 srfi-36 srfi-37
 	srfi-39 srfi-40 srfi-42 srfi-43 srfi-45
-        srfi-60 srfi-61 srfi-62 srfi-66 srfi-67))
+        srfi-60 srfi-61 srfi-62 srfi-66 srfi-67
+	srfi-74))
 
     ; Some SRFI's redefine Scheme variables.
     (define shadowed
@@ -627,5 +628,44 @@
 	)
   (files srfi-67))
 
-	
-	  
+; SRFI 74: Octet-addressed binary objects
+
+(define-interface srfi-74-interface
+  (export (endianness :syntax)
+	  blob? make-blob
+	  blob-length
+	  blob-u8-ref blob-u8-set!
+	  blob-s8-ref blob-s8-set!
+	  blob-uint-ref blob-uint-set!
+	  blob-sint-ref blob-sint-set!
+
+	  blob-u16-ref blob-s16-ref
+	  blob-u16-native-ref blob-s16-native-ref
+	  blob-u16-set! blob-s16-set!
+	  blob-u16-native-set! blob-s16-native-set!
+
+	  blob-u32-ref blob-s32-ref
+	  blob-u32-native-ref blob-s32-native-ref
+	  blob-u32-set! blob-s32-set!
+	  blob-u32-native-set! blob-s32-native-set!
+
+	  blob-u64-ref blob-s64-ref
+	  blob-u64-native-ref blob-s64-native-ref
+	  blob-u64-set! blob-s64-set!
+	  blob-u64-native-set! blob-s64-native-set!
+
+	  blob=?
+	  blob-copy blob-copy!
+
+	  blob->u8-list u8-list->blob
+	  blob->uint-list blob->sint-list 
+	  uint-list->blob sint-list->blob))
+
+(define-structure srfi-74 srfi-74-interface
+  (open scheme
+	srfi-23 ; ERROR
+	srfi-26 ; CUT
+	bitwise
+	srfi-66 ; U8VECTOR
+	)
+  (files srfi-74))
