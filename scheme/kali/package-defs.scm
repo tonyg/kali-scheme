e9f5d79c502c627aee8c922aef14434ca1bf56cf
hetz <none@none>
2007-08-18 12:30:44 -0400

    Subject: the kali utils got their own file now

--- scheme/kali/package-defs.scm
+++ scheme/kali/package-defs.scm
@@ -196,33 +196,4 @@
 	disclose-aspaces ;; just to make shure it loads
 	messages proxy-internals threads
 	fluids-internal)		; current-thread
-  (begin
-    ; Little proxy utilities.
-
-    (define (proxy-owner proxy)
-      (or (proxy-data-owner (proxy-data proxy))
-	  (local-address-space)))
-
-    (define (proxy-remote-ref proxy)
-      (remote-apply (proxy-owner proxy) proxy-local-ref proxy))
-
-    (define (proxy-remote-set! proxy value)
-      (remote-run! (proxy-owner proxy) proxy-local-set! proxy value))
-
-    ; To avoid circular module dependencies we provide PROXY-REMOTE-REF to
-    ; the rts low module.
-    
-    (initialize-any-proxy-value! proxy-remote-ref)
-    
-    ; Move the current thread to another aspace.
-    (define move-to!
-      (lambda (aspace)
-	(call-with-current-continuation
-	  (lambda (k)
-	    (remote-run! aspace
-			 spawn
-			 (lambda ()
-			   (k #F))
-			 (thread-name (current-thread)))
-	    (terminate-current-thread)))))
-    ))
+  (files utils))
---
+++ scheme/kali/utils.scm
@@ -0,0 +1,45 @@
+; Little proxy utilities.
+
+(define (proxy-owner proxy)
+  (or (proxy-data-owner (proxy-data proxy))
+      (local-address-space)))
+
+(define (proxy-remote-ref proxy)
+  (remote-apply (proxy-owner proxy) proxy-local-ref proxy))
+
+(define (proxy-remote-set! proxy value)
+  (remote-run! (proxy-owner proxy) proxy-local-set! proxy value))
+
+;; Handles are shared global data structures
+
+(define make-handle make-proxy)
+
+(define handle-owner proxy-owner)
+
+(define (set-handle! handle value)
+  (remote-apply (handle-owner handle)
+		set-proxy-value! 
+		handle 
+		value))
+
+(define (handle-value handle)
+  (remote-apply (handle-owner handle)
+		proxy-value 
+		handle))
+
+; To avoid circular module dependencies we provide PROXY-REMOTE-REF to
+; the rts low module.
+    
+(initialize-any-proxy-value! proxy-remote-ref)
+    
+; Move the current thread to another aspace.
+(define move-to!
+  (lambda (aspace)
+    (call-with-current-continuation
+     (lambda (k)
+       (remote-run! aspace
+		    spawn
+		    (lambda ()
+		      (k #F))
+		    (thread-name (current-thread)))
+       (terminate-current-thread)))))
