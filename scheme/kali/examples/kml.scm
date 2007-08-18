039073a08a48e54ae260092d46e04474c65117cf
hetz <none@none>
2007-08-18 12:39:09 -0400

    Subject: image depends from kali/examples/*.scm. other was a typo and some comment stuff.

--- scheme/kali/examples/kml.scm
+++ scheme/kali/examples/kml.scm
@@ -26,24 +26,24 @@
 	   (channel? (proxy-local-ref p)))
 	 channel)))
 
-;; kml-channel message -> rendezvous
+;; :: kml-channel message -> rendezvous
 (define kml-send-rv 
-  (make-proxy-remote-funk send-rv
+  (make-proxy-remote-func send-rv
 			  channel 
 			  message))
 
-;; kml-channel message -> unspecific (my block)
+;; :: kml-channel message -> unspecific (my block)
 (define kml-send
   (make-proxy-remote-func send
 			  channel 
 			  message))
 
-;; kml-channel -> rendezvous
+;; :: kml-channel -> rendezvous
 (define kml-receive-rv 
   (make-proxy-remote-func receive-rv
 			  channel))
 
-;; kml-channel -> value (may block)
+;; :: kml-channel -> value (may block)
 (define kml-receive
   (make-proxy-remote-func receive
 			  channel))
@@ -53,11 +53,11 @@
 ;; async-channels:
 ;; ===============
 
-;; -> async-kml-channel
+;; :: -> async-kml-channel
 (define make-kml-async-channel
   (make-proxy (make-async-channel)))
 
-;; obj -> boolean
+;; :: obj -> boolean
 (define (kml-async-channel? channel)
   (and (proxy? channel)
        (remote-apply (proxy-owner channel)
@@ -65,18 +65,18 @@
 	   (async-channel? (proxy-local-ref p)))
 	 channel)))
 
-;; async-kml-channel message -> unspecific
+;; :: async-kml-channel message -> unspecific
 (define kml-send-async
   (make-proxy-remote-func send-async
 			  channel
 			  message))
 
-;; async-kml-channel -> rendezvous
+;; :: async-kml-channel -> rendezvous
 (define kml-receive-async-rv
   (make-proxy-remote-func receive-async-rv
 			  channel))
 			  
-;; async-kml-channel -> value (may block)
+;; :: async-kml-channel -> value (may block)
 (define kml-receive-async
   (make-proxy-remote-func receive-async
 			  channel))
@@ -86,11 +86,11 @@
 ;; placeholders:
 ;; =============
 
-;; -> empty-placeholder
+;; :: -> empty-placeholder
 (define (make-kml-placeholder)
   (make-proxy (make-placeholder)))
 
-;; object -> boolean
+;; :: object -> boolean
 (define (kml-placeholder? placeholder)
   (and (proxy? placeholder)
        (remote-apply (proxy-owner placeholder)
@@ -98,17 +98,17 @@
 	   (placeholder? (proxy-local-ref p)))
 	 placeholder)))
 
-;; placeholder -> rendezvous
+;; :: placeholder -> rendezvous
 (define kml-placeholder-value-rv
   (make-proxy-remote-func placeholder-value-rv
 			  placeholder))
 
-;; placeholder -> value (may block)
+;; :: placeholder -> value (may block)
 (define kml-placeholder-value
   (make-proxy-remote-func placeholder-value
 			  placeholder))
 
-;; placeholder value -> unspecific
+;; :: placeholder value -> unspecific
 (define kml-placeholder-set!
   (make-proxy-remote-func placeholder-set!
 			  placeholder))
