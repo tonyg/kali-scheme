73477920b552367790b8f8bc6e90042c74836405
hetz <none@none>
2007-08-18 01:55:19 -0400

    the kali interface

--- scheme/kali/package-defs.scm
+++ scheme/kali/package-defs.scm
@@ -155,3 +155,56 @@
   (files message))
 
 ;; =======================================================================
+
+(define-interface kali-interface
+  (export socket-id->address-space
+	  address-space?
+
+	  remote-run!
+	  remote-apply
+
+	  make-proxy
+	  proxy?
+	  proxy-owner
+	  proxy-local-ref proxy-local-set!
+	  proxy-remote-ref proxy-remote-set!
+	  any-proxy-value
+
+	  move-to!
+
+	  start-server))
+
+(define-structure kali kali-interface
+  (open scheme address-spaces
+	messages proxy-internals threads
+	fluids-internal)		; current-thread
+  (begin
+    ; Little proxy utilities.
+
+    (define (proxy-owner proxy)
+      (or (proxy-data-owner (proxy-data proxy))
+	  (local-address-space)))
+
+    (define (proxy-remote-ref proxy)
+      (remote-apply (proxy-owner proxy) proxy-local-ref proxy))
+
+    (define (proxy-remote-set! proxy value)
+      (remote-run! (proxy-owner proxy) proxy-local-set! proxy value))
+
+    ; To avoid circular module dependencies we provide PROXY-REMOTE-REF to
+    ; the rts low module.
+    
+    (initialize-any-proxy-value! proxy-remote-ref)
+    
+    ; Move the current thread to another aspace.
+    (define move-to!
+      (lambda (aspace)
+	(call-with-current-continuation
+	  (lambda (k)
+	    (remote-run! aspace
+			 spawn
+			 (lambda ()
+			   (k #F))
+			 (thread-name (current-thread)))
+	    (terminate-current-thread)))))
+    ))
