48466e4a2f37de032461044f3073b89511e50049
hetz <none@none>
2007-08-18 01:59:53 -0400

    Subject: fluid-test

--- scheme/kali/tests/fluids.scm
+++ scheme/kali/tests/fluids.scm
@@ -1,3 +1,58 @@
-
+     
 (define (fluid-test machine-name port)
-  #t)
+  (let ((other-aspace (socket-id->address-space machine-name
+						port)))
+    (really-fluid-test)
+    (remote-run! other-aspace really-fluid-test)))
+
+(define (really-fluid-test)
+  (let (($mumble (make-fluid 0)))
+    (check (let ((a (fluid $mumble))
+		 (b (let-fluid $mumble 1
+			       (lambda () (fluid $mumble))))
+		 (c (fluid $mumble))
+		 (d (let-fluid $mumble 2
+			       (lambda ()
+				 (let-fluid $mumble 3
+					    (lambda () (fluid $mumble)))))))
+	     (list a b c d))
+	   => '(0 1 0 3))
+     
+    (check (fluid $mumble)
+	   => 0)			;0
+
+    (let-fluid $mumble 1 
+	       (lambda () 
+		 (check (fluid $mumble)
+			=> 1)))		;1
+
+    (check (fluid $mumble)
+	   => 0)			;0
+
+    (let-fluid $mumble 1
+	       (lambda ()
+		 (check (fluid $mumble)
+			=> 1)		;1
+		 (let-fluid $mumble 2 
+			    (lambda () 
+			      (check (fluid $mumble)
+				     => 2))))) ;2
+    (check (fluid $mumble) 
+	   => 0)			;0
+
+    ((call-with-current-continuation
+      (lambda (k)
+	(lambda ()
+	  (let-fluid $mumble 1
+		     (lambda ()
+		       (check (fluid $mumble)
+			      => 1)	;1
+		       (let-fluid $mumble 2
+				  (lambda () 
+				    (check (fluid $mumble)
+					   => 2))) ;2
+		       (k (lambda () 
+			    (check (fluid $mumble)
+				   => 0))))))))) ;0
+    (check (fluid $mumble)
+	   => 0)))			;0
