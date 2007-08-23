a25c5cb7031343d73fe1227b16bd44acf4d8f959
Mike Sperber <sperber@deinprogramm.de>
2007-08-23 17:23:32 +0200

    Backed out changeset 95d76814a6bc
    Contains changes to unrelated files.

--- scheme/rts/encoding.scm
+++ scheme/rts/encoding.scm
@@ -220,17 +220,24 @@
 	      (loop (cons char rev-chars)
 		    (+ 1 char-count)
 		    (+ count source-index)))
+	     ((not count)
+	      (if maybe-error-char
+		  (loop (cons maybe-error-char rev-chars)
+			(+ 1 char-count)
+			(+ 1 source-index))
+		  (decoding-error enc	; ####
+				  "invalid encoding"
+				  source (+ start source-index))))
+	     ;; it's incomplete from here
 	     (maybe-error-char
-	      (loop (cons maybe-error-char rev-chars)
-		    (+ 1 char-count)
-		    (+ 1 source-index)))
-	     (count
-	      (decoding-error enc	; ####
-			      "incomplete encoding"
-			      source (+ start source-index)))
+	      (let loop ((error-char-count (- source-count count))
+			 (rev-chars '()))
+		(if (zero? error-char-count)
+		    (reverse-list->string rev-chars (+ 1 char-count))
+		    (loop (- error-char-count 1) (cons maybe-error-char rev-chars)))))
 	     (else
 	      (decoding-error enc	; ####
-			      "invalid encoding"
+			      "incomplete encoding"
 			      source (+ start source-index)))))))))
 
 ))))
