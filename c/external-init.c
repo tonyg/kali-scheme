4c69958e3f8f28f86bb4440f7d36f49d5caa4e3d
Mike Sperber <sperber@deinprogramm.de>
2007-09-19 10:39:30 +0200

    Zap build-external-modules.
    Remove the rudimentary infrastructure for extending the set of
    external modules statically linked into the Scheme 48 VM.  It doesn't
    seem anybody ever used it; dynamic externals are the way to go,
    anyway.

---
+++ c/external-init.c
@@ -0,0 +1,24 @@
+/* Copyright (c) 1993-2007 by Richard Kelsey and Jonathan Rees.
+   See file COPYING. */
+
+/* Initialize external modules. */
+
+extern void s48_initialize_external(void);
+extern void s48_init_external_libs(void);
+extern void s48_init_socket(void);
+extern void s48_init_dynlink(void);
+extern void s48_init_sysexits(void);
+extern void s48_init_srfi_27(void);
+extern void s48_init_asm_glue(void);
+
+void
+s48_initialize_external_modules(void)
+{
+  s48_initialize_external();
+  s48_init_external_libs();
+  s48_init_socket();
+  s48_init_dynlink();
+  s48_init_sysexits();
+  s48_init_srfi_27();
+  s48_init_asm_glue();
+}
