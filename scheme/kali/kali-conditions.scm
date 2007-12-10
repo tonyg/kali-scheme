e35c8a1cb65e0167e9eda88210a890339f83daf8
Michael Olson <mwolson@member.fsf.org>
2007-12-09 23:23:05 -0500

    scheme/kali/kali-conditions.scm: Use new syntax for define-condition-type.

--- scheme/kali/kali-conditions.scm
+++ scheme/kali/kali-conditions.scm
@@ -40,29 +40,29 @@
 ;;
 
 (define-condition-type &kali-error &error
-  kali-error?)
+  make-kali-error kali-error?)
 
 ;; --------------
 ;; reader
 (define-condition-type &kali-reader-error &kali-error
-  kali-reader-error?
+  make-kali-reader-error kali-reader-error?
   (channel kali-reader-error-channel))
 
 (define-condition-type &kali-reader-eof-error &kali-reader-error
-  kali-reader-eof-error?)
+  make-kali-reader-eof-error kali-reader-eof-error?)
 
 (define-condition-type &kali-reader-insufficient-error &kali-reader-error
-  kali-reader-insufficient-error?
+  make-kali-reader-insufficient-error kali-reader-insufficient-error?
   (got     kali-reader-insufficient-error-got)
   (length  kali-reader-insufficient-error-length))
 
 (define-condition-type &kali-reader-condvar-error &kali-reader-error
-  kali-reader-condvar-error?)
+  make-kali-reader-condvar-error kali-reader-condvar-error?)
 
 ;; -----------
 ;; remote
 (define-condition-type &kali-remote-error &kali-error
-  kali-remote-error?
+  make-kali-remote-error kali-remote-error?
   (aspace    kali-remote-error-aspace)
   (procedure kali-remote-error-procedure)
   (arguments kali-remote-error-arguments)
@@ -74,37 +74,37 @@
 ;; -----------
 ;; send
 (define-condition-type &kali-send-message-to-self-error &kali-error
-  kali-send-message-to-self-error?
+  make-kali-send-message-to-self-error kali-send-message-to-self-error?
   (type kali-send-message-to-self-error-type)
   (message kali-send-message-to-self-error-message))
 
 ;; -----------
 ;; connect
 (define-condition-type &kali-connect-error &kali-error
-  kali-connect-error?
+  make-kali-connect-error kali-connect-error?
   (need-counts kali-connect-error-need-counts))
 
 ;; ----------
 ;; memory
 (define-condition-type &kali-memory-layout-error &kali-error
-  kali-memory-layout-error?
+  make-kali-memory-layout-error kali-memory-layout-error?
   (alien-vector kali-memory-layout-error-alien-vector)
   (local-vector kali-memory-layout-error-local-vector))
 
 ;; ----------
 ;; server
 (define-condition-type &kali-no-server-started-error &kali-error
-  kali-no-server-started-error?)
+  make-kali-no-server-started-error kali-no-server-started-error?)
 
 (define-condition-type &kali-server-already-started-error &kali-error
-  kali-server-already-started-error?)
+  make-kali-server-already-started-error kali-server-already-started-error?)
 
 ;; ----------
 ;; bad-stob/object
 (define-condition-type &kali-bad-stob-type-error &kali-error
-  kali-bad-stob-type-error?
+  make-kali-bad-stob-type-error kali-bad-stob-type-error?
   (type kali-bad-stob-type-error-type))
 
 (define-condition-type &kali-bad-object-error &kali-error
-  kali-bad-object-error?
+  make-kali-bad-object-error kali-bad-object-error?
   (object kali-bad-object-error-object))
